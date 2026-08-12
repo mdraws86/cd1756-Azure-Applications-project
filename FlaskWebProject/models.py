e1b66a4d634c56eadb626701d19b7fe65673e93c
mdraws86 <m.draws@hotmail.de>
2026-08-12 06:19:52 +0200

    BlobServiceClient

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -2,13 +2,21 @@ from datetime import datetime
 from FlaskWebProject import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from azure.storage.blob import BlockBlobService
+from azure.storage.blob import BlobServiceClient#BlockBlobService
 import string, random
 from werkzeug.utils import secure_filename
 from flask import flash
 
 blob_container = app.config['BLOB_CONTAINER']
-blob_service = BlockBlobService(account_name=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
+#BlockBlobService(account_name=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
+account_url = f"https://{app.config['BLOB_ACCOUNT']}.blob.core.windows.net"
+blob_service = BlobServiceClient(
+    account_url=account_url,
+    credential={
+        "account_name": app.config['BLOB_ACCOUNT'],
+        "account_key": app.config['BLOB_STORAGE_KEY'],
+    },
+)
 
 def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -51,18 +59,33 @@ class Post(db.Model):
         self.body = form.body.data
         self.user_id = userId
 
+        #if file:
+        #    filename = secure_filename(file.filename);
+        #    fileextension = filename.rsplit('.',1)[1];
+        #    Randomfilename = id_generator();
+        #    filename = Randomfilename + '.' + fileextension;
+        #    try:
+        #        blob_service.create_blob_from_stream(blob_container, filename, file)
+        #        if(self.image_path):
+        #            blob_service.delete_blob(blob_container, self.image_path)
+        #    except Exception:
+        #        flash(Exception)
+        #    self.image_path =  filename
         if file:
-            filename = secure_filename(file.filename);
-            fileextension = filename.rsplit('.',1)[1];
+            filename = secure_filename(file.filename)
+            fileextension = filename.rsplit('.',1)[1]
             Randomfilename = id_generator();
-            filename = Randomfilename + '.' + fileextension;
+            filename = Randomfilename + '.' + fileextension
             try:
-                blob_service.create_blob_from_stream(blob_container, filename, file)
-                if(self.image_path):
-                    blob_service.delete_blob(blob_container, self.image_path)
-            except Exception:
-                flash(Exception)
-            self.image_path =  filename
+                blob_client = blob_service.get_blob_client(container=blob_container, blob=filename)
+                blob_client.upload_blob(file, overwrite=True)
+
+                if self.image_path:
+                    old_blob_client = blob_service.get_blob_client(container=blob_container, blob=self.image_path)
+                    old_blob_client.delete_blob()
+            except Exception as e:
+                flash(str(e))
+            self.image_path = filename
         if new:
             db.session.add(self)
         db.session.commit()
